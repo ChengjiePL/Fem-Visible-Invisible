64781e4a078f658ebbc55f3e55c8b6b5c8fe1e18
PaDi224 <paudiazma@gmail.com>
2024-12-14 20:19:55 +0100

    Resultats(0.2628)
    S'ha implementat una "poda" que calcula la distància de cada estació objectiu respecte a les altres, la passa a metres i, poda totes les que tinguin una distància superior a la mitja.

--- Backend/R/Scripts/main.R
+++ Backend/R/Scripts/main.R
@@ -1,11 +1,11 @@
 library(ncdf4)
-
+library(pracma)
 wdir <- "../Data/"
 
 #PRUEBAS PROPIAS
 #Info del tipus de part de la ciutat que hi ha en una (lat, lon) concreta
 infoEstacions <- read.csv("XVPCA_info_sconcno2_2023.csv")
-infoEstacions <- head(infoEstacions,68)
+infoEstacions <- head(infoEstacions, length(infoEstacions$code))
 taulaInfoEstacions <- table(infoEstacions)
 View(taulaInfoEstacions)
 
@@ -13,28 +13,74 @@ View(taulaInfoEstacions)
 mesuresEstacions <- read.csv("gene_sconcno2_2023_xvpca_emep_port.csv")
 View(mesuresEstacions)
 variables = mesuresEstacions[0, ]
-estacions = c(41.39216, 2.009802) #Aquí aniran les dades de les estacions objectiu a mesurar mostrades al kaggle
+estacions = list(c(41.39216, 2.009802), 
+                 c(41.11588, 1.191975),
+                 c(41.44398, 2.237875),
+                 c(41.32177, 2.082141)) #Aquí aniran les dades de les estacions objectiu a mesurar mostrades al kaggle
 dades = c("date", "lat", "lon", "concentration") #Columnes que ha de tenir el csv a entregar
-resultats <- data.frame(Date=as.character(), #Creem DataFrame on mostrar els resultats
+resultats <- data.frame(id = integer(),
+                        date=as.character(), #Creem DataFrame on mostrar els resultats
                         lat=double(), 
                         lon=double(), 
                         concentration=double()) 
 View(resultats)
+nearest <- list(c(), c(), c(), c())
+
+for(i in 1:length(infoEstacions$code))
+{
+  for(j in 1:length(estacions))
+  {
+    dist = haversine(c(estacions[[j]][1], estacions[[j]][2]), c(infoEstacions[i,][["lat"]], infoEstacions[i,][["lon"]]))
+    nearest[[j]][length(nearest[[j]])+1] = infoEstacions[i,][["code"]]
+    nearest[[j]][length(nearest[[j]])+1] = dist
+  }
+}
+
+nearestCodes <- list(c(), c(), c(), c())
+for(i in 1:length(nearest))
+{
+  mitja = mean(as.double(nearest[[i]][seq(2,length(nearest[[i]]), by=2)]))
+  for(j in seq(2, length(nearest[[1]]), by=2))
+  {
+    if(nearest[[i]][j] <= mitja)
+    {
+      nearestCodes[[i]][length(nearestCodes[[i]])+1] <- nearest[[i]][j-1] 
+    }
+  }
+}
+
 cl = colnames(mesuresEstacions)[0:-1] #Obtenim  el nom totes les estacions per poder obtenir les mesures
 for(i in 1:nrow(mesuresEstacions))
 {
-  sum = 0.0
-  for(j in cl){
-    if(!is.na(mesuresEstacions[[j]][i]))
-    {    
-      sum = sum + mesuresEstacions[[j]][i]
-      
+  for(z in 1:length(estacions))
+  {
+    sum = 0.0
+    for(j in cl){
+      if(!is.na(mesuresEstacions[[j]][i]) && is.element(j, nearestCodes[[z]]))
+      {    
+        total = mean( mesuresEstacions[[j]][i] + mesuresEstacions[[j]][i - 1] + mesuresEstacions[[j]][i + 1])
+        sum = sum + total
+        
+      }
     }
+    conc = sum/length(cl)
+    if(is.na(conc))
+    {
+      conc = 16.5
+    }
+    index = (i-1) * 4 + z
+    resultats[index,] <- list(index, mesuresEstacions$Date[i],
+                          estacions[[z]][1],
+                          estacions[[z]][2],
+                          conc)
   }
-  conc = sum/length(cl)
-  resultats[i,] <- list(mesuresEstacions$Date[i],
-                        estacions[1],
-                        estacions[2],
-                        conc)
 }
-  
+
+X = resultats$date
+X = as.factor(X)
+Y = resultats$concentration
+dev.new() ; msgWindow(type="maximize") #Fem un resize de la window per mostrar la gràfica
+grafic = plot(X, Y, xlab = "date", ylab= "Contamination", ylim= c(0,50), las=2)
+
+
+write.csv(resultats, "./resultats.csv", quote = FALSE, row.names = FALSE)
